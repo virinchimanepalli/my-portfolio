28ee8b544e11951a9c54f44944e178534ac604f6
virinchy25 <virinchi@sparkstudio.co>
2022-10-24 13:00:48 +0530

    [fix]: changed location.

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
   email: "virinchi.msk83@gmail.com",
   phone: "+91-8143242514",
-  address: "Jp Nagar, Bangalore",
+  address: "Bangalore, India",
   sheetAPI: "",
 };
