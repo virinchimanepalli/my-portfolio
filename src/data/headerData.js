0f8a541b06296dd3e818626788a8301d47c4c383
virinchy25 <virinchi@sparkstudio.co>
2023-04-23 21:02:45 +0530

    chnaged drive link for resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     "Frontend developer having two plus years of experience in helping startup's to grow.I like to tackle problems and solve them in the best Javascripty way.",
   image: "https://i.ibb.co/Vv8JRFq/virinchi-dp.jpg",
   resumePdf:
-    "https://drive.google.com/file/d/1MSFCvlFSkC_RhqSmK7pS-eM9sYwZnNjK/view?usp=sharing",
+    "https://drive.google.com/file/d/1PK8nwQra6yu3FcT4DooT_QaK_7Rs2TaX/view",
 };
